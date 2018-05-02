e0f13ed691938842a8778f11a178bfb169596b39
aidangerard <36491983+aidangerard@users.noreply.github.com>
2018-05-02 16:17:34 -0700

    Add files via upload

--- debug/moc_addcustomer.cpp
+++ debug/moc_addcustomer.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'addcustomer.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'addcustomer.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.10.1. It"
+#error "This file was generated using the moc from 5.8.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -80,9 +80,9 @@ void AddCustomer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject AddCustomer::staticMetaObject = {
+const QMetaObject AddCustomer::staticMetaObject = {
     { &QDialog::staticMetaObject, qt_meta_stringdata_AddCustomer.data,
-      qt_meta_data_AddCustomer,  qt_static_metacall, nullptr, nullptr}
+      qt_meta_data_AddCustomer,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
@@ -93,9 +93,9 @@ const QMetaObject *AddCustomer::metaObject() const
 
 void *AddCustomer::qt_metacast(const char *_clname)
 {
-    if (!_clname) return nullptr;
+    if (!_clname) return Q_NULLPTR;
     if (!strcmp(_clname, qt_meta_stringdata_AddCustomer.stringdata0))
-        return static_cast<void*>(this);
+        return static_cast<void*>(const_cast< AddCustomer*>(this));
     return QDialog::qt_metacast(_clname);
 }
 
--- debug/moc_moreinformation.cpp
+++ debug/moc_moreinformation.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'moreinformation.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'moreinformation.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.10.1. It"
+#error "This file was generated using the moc from 5.8.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -76,9 +76,9 @@ void MoreInformation::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject MoreInformation::staticMetaObject = {
+const QMetaObject MoreInformation::staticMetaObject = {
     { &QDialog::staticMetaObject, qt_meta_stringdata_MoreInformation.data,
-      qt_meta_data_MoreInformation,  qt_static_metacall, nullptr, nullptr}
+      qt_meta_data_MoreInformation,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
@@ -89,9 +89,9 @@ const QMetaObject *MoreInformation::metaObject() const
 
 void *MoreInformation::qt_metacast(const char *_clname)
 {
-    if (!_clname) return nullptr;
+    if (!_clname) return Q_NULLPTR;
     if (!strcmp(_clname, qt_meta_stringdata_MoreInformation.stringdata0))
-        return static_cast<void*>(this);
+        return static_cast<void*>(const_cast< MoreInformation*>(this));
     return QDialog::qt_metacast(_clname);
 }
 
